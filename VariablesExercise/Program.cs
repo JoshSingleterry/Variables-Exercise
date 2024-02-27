c6fc456385bb51684d7c725d7f9863b6232c812a
JoshSingleterry <jsingleterry12@gmail.com>
2024-02-27 11:22:57 -0600

    finished

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// Declaring and initialize
+string name = "Josh";
+int age = 32;
+char middleInitial = 'M';
+bool isMarried = true;
+double currentTemp = 60;
+decimal priceOfgas = 2.78m;
+
+Console.WriteLine($"Hello, my name is {name} and I am {age} years old");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Married: {isMarried}");
+Console.WriteLine($"Current Temperature: {currentTemp}");
+Console.WriteLine($"Price of gas today: {priceOfgas}");
+
